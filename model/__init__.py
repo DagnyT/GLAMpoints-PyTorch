d8a7c82f2098e40a5abaf75b566ff3a5b4e5a12e
Anastasiia Mishchuk <anastasiya.mishchuk@gmail.com>
2020-02-07 18:39:48 +0100

    Update __init__.py
    Minor fix

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,3 @@
-from .model_modified import Unet_model_4 as Unet_model_4_modified
 from .model import Unet_model_4
 from .loss import Reward_Loss
 
@@ -6,8 +5,6 @@ def build_model(cfg):
 
     if cfg['TRAINING']['MODEL'] == 'init':
         model = Unet_model_4(cfg['TRAINING']['INPUT_LAYER'])
-    else:
-        model = Unet_model_4_modified(cfg['TRAINING']['INPUT_LAYER'])
 
     return model
 
